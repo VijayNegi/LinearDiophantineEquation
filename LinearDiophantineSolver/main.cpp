9ab57d48276e3dd3e95a25a5846d496b09d9251b
VIJAY SINGH NEGI <vinegi@adobe.com>
2021-11-04 15:49:49 +0530

    Initial Commit

---
+++ LinearDiophantineSolver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinearDiophantineSolver
+//
+//  Created by Vijay Singh negi on 04/11/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
